67bebd7d4130b02a380cc2ffea7152104bf86782
danofriev <denchik.anofriev@gmail.com>
2024-01-07 03:27:40 +0300

    Improvements for better working

--- scripts/popup.js
+++ scripts/popup.js
@@ -1,8 +1,18 @@
 
 document.addEventListener('DOMContentLoaded', function() {
-  document.getElementById('changeBackgroundButton').addEventListener('click', function() {
+  document.getElementById('unlockPremiumStatistics').addEventListener('click', function() {
     chrome.tabs.query({ active: true, currentWindow: true }, function(tabs) {
       chrome.tabs.sendMessage(tabs[0].id, { action: 'unlockPremiumStatistics' });
     });
+  });
+});
+
+document.addEventListener('DOMContentLoaded', function () {
+  const openNewTabLink = document.getElementById('openNewTabLink');
+
+  openNewTabLink.addEventListener('click', function (event) {
+    event.preventDefault();
+
+    chrome.tabs.create({ url: 'https://github.com/anof1r/faceit_stats_unlocker' });
   });
 });
